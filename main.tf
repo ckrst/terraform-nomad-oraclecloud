f5eed2006c11185da5c43c5090167be8b7e3a440
Vinicius Kirst <vinicius.kirst@gmail.com>
2021-08-01 21:58:54 -0300

    private key

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ terraform {
 }
 
 provider "oci" {
-  tenancy_ocid     = var.oracle_tenancy_ocid
-  user_ocid        = var.oracle_user_ocid
-  fingerprint      = var.oracle_fingerprint
-  private_key_path = var.oracle_private_key_path
-  region           = var.oracle_region
+  tenancy_ocid = var.oracle_tenancy_ocid
+  user_ocid = var.oracle_user_ocid
+  fingerprint = var.oracle_fingerprint
+  private_key = var.oracle_private_key
+  region = var.oracle_region
 }
 
 data "oci_core_image" "ubuntu_image" {
--- variables.tf
+++ variables.tf
@@ -1,7 +1,8 @@
 variable "oracle_tenancy_ocid" {}
 variable "oracle_user_ocid" {}
 variable "oracle_fingerprint" {}
-variable "oracle_private_key_path" {}
+# variable "oracle_private_key_path" {}
+variable "oracle_private_key" {}
 variable "oracle_region" {}
 variable "oracle_account_email" {}
 variable "oracle_compartment_id" {}
