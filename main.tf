6b935f78e0743b9be525c8a9d4bd67f5c2da5b8c
Vinicius Kirst <vinicius.kirst@gmail.com>
2021-08-01 22:39:52 -0300

    subnet

--- main.tf
+++ main.tf
@@ -35,3 +35,27 @@ resource "oci_core_vcn" "nomad_vcn" {
     is_ipv6enabled = false
     timeouts {}
 }
+
+resource "oci_core_subnet" "nomad_subnet" {
+    #Required
+    cidr_block = "10.0.0.0/24"
+    compartment_id = var.oracle_compartment_id
+    vcn_id = oci_core_vcn.nomad_vcn.id
+
+    #Optional
+    display_name = "NomadSubnet"
+    dns_label = "nomadsubnet"
+
+    defined_tags = {
+      "Oracle-Tags.CreatedBy" = "oracleidentitycloudservice/vinicius.kirst@gmail.com"
+      # "Oracle-Tags.CreatedOn" = "2021-04-09T05:17:56.745Z"
+    }
+    dhcp_options_id = oci_core_dhcp_options.nomad_dhcp_options.id
+    freeform_tags = {}
+    prohibit_internet_ingress = false
+    prohibit_public_ip_on_vnic = false
+    route_table_id = oci_core_route_table.nomad_route_table.id
+    security_list_ids = [
+      oci_core_security_list.nomad_security_list.id,
+    ]
+}
